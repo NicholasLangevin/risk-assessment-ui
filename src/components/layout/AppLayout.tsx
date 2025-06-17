bdb7b499caaf6716f200d7be6bbceb23146d8c2b
nicholas langevin <nic.skate22@gmail.com>
2025-06-17 04:47:20 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Sidebar collapsib

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -2,9 +2,7 @@
 import type React from 'react';
 import Link from 'next/link';
 import {
-  // SidebarProvider, // Removed from here
   Sidebar,
-  SidebarHeader,
   SidebarContent,
   SidebarMenu,
   SidebarMenuItem,
@@ -19,13 +17,9 @@ interface AppLayoutProps {
 
 export function AppLayout({ children }: AppLayoutProps) {
   return (
-    // SidebarProvider removed from here
       <div className="flex flex-1"> {/* This div helps manage the layout below the global Header */}
         <Sidebar collapsible="icon" className="h-[calc(100vh-3.5rem)] fixed top-14 left-0 z-30"> {/* Adjust height and position if Header is 3.5rem (h-14) */}
-          <SidebarHeader className="p-4 flex items-center group-data-[state=expanded]:justify-start group-data-[state=collapsed]:justify-center">
-            {/* Content for the sidebar's own header, e.g., logo or title when expanded */}
-             <span className="font-bold group-data-[state=expanded]:inline-block group-data-[state=collapsed]:hidden">RiskPilot</span>
-          </SidebarHeader>
+          {/* SidebarHeader removed */}
           <SidebarContent>
             <SidebarMenu>
               <SidebarMenuItem>
@@ -53,6 +47,5 @@ export function AppLayout({ children }: AppLayoutProps) {
           </main>
         </SidebarInset>
       </div>
-    // SidebarProvider removed from here
   );
 }
